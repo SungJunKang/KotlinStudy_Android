797416bc9f9287f8c081c70567213c88ab644266
SungJunKang <std07209@naver.com>
2018-01-17 18:59:37 +0900

    KotlinStudy_Android_2

---
+++ Kotlin_App_2/app/src/main/kotlin/SecondActivity.kt
@@ -0,0 +1,11 @@
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.ana.kotlin_app_2.R
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
